363d8e21bded2070ff03dcd19e4f9767779bf0ea
Lei0Lu <9.lei.ly.9@gmail.com>
2022-10-01 22:46:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+using _20._09._2022;
+
+
+
+
+
+
+
+Car[] cars = new Car[]
+{
+    new Car("Japan_Life", "Mercedes-Benz Atego 815", 100, TypeCar.cargo),
+    new Car("Japan_Life", "VOLVO FMX-500-10X4", 90, TypeCar.cargo),
+    new Car("Lamborghini", "Diablo GTR", 340, TypeCar.passenger),
+    new Car("McLaren", "720S", 341, TypeCar.passenger),
+    new Car("Iveco Defense Vehicles", "LMV", 130, TypeCar.military),
+    new Car("Bibika", "Vruum", 9999, TypeCar.undefined)
+};
+
+Console.WriteLine("Вывести все машины на экран ");
+foreach (Car car in cars)
+{
+    car.ToString();
+}
+
+Console.WriteLine("Вывести сумму скоростей машин");
+double sum=0;
+foreach (Car car in cars)
+{
+    sum+=car.Speed;
+}
+Console.WriteLine(sum);
+
+Console.WriteLine("Вывести ТОЛЬКО грузовые машины из массива");
+
+foreach (Car car in cars)
+{
+    if(car.Type == TypeCar.cargo)
+    {
+        car.ToString();
+    }
+}
+
+
+
+
+
+
+
+
+
+
---
+++ Сar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._09._2022
+{
+    internal class Car
+    {
+
+        public string Сompany { get; set; }
+        public string Model { get; set; }
+        public double Speed { get; set; }
+        public TypeCar Type { get; set; }
+
+
+        public Car(string company, string model, double speed, TypeCar type)
+        {
+            Сompany = company;
+            Model = model;
+            Speed = speed;
+            Type = type;
+
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{Model} Компании: {Сompany}. Скорость: {Speed}/ Тип: {Type}");
+        }
+
+
+    }
+    enum TypeCar
+    {
+
+        cargo,
+        passenger,
+        military,
+        undefined
+
+    }
+
+}
